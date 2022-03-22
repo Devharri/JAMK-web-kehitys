3d39dbeda9d8966132e902ec6241e3d0d1ba6ee4
Devharri <devharri@outlook.com>
2022-03-22 21:05:14 +0200

    vaihdettu valikko responsiivisempaan

--- web_tekniikat/harjoitustyo/JS/scripts.js
+++ web_tekniikat/harjoitustyo/JS/scripts.js
@@ -17,3 +17,13 @@
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   }
+
+//Navbar functions
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
