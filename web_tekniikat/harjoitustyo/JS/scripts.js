391961b25931956a8b51dc570f74963928c4b8d7
Devharri <devharri@outlook.com>
2022-03-23 01:09:46 +0200

    added shadow to TopButton

--- web_tekniikat/harjoitustyo/JS/scripts.js
+++ web_tekniikat/harjoitustyo/JS/scripts.js
@@ -1,24 +1,20 @@
-  //Get the button
-  var mybutton = document.getElementById("goTopBtn");
+// <GotoTop-button>
+//Get the button
+var mybutton = document.getElementById("goTopBtn");
 
-  // When the user scrolls down 20px from the top of the document, show the button
-  window.onscroll = function() {scrollFunction()};
-  
-  function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
-  }
-  
-  // When the user clicks on the button, scroll to the top of the document
-  function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
 
-//Navbar functions
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// </GotoTop-button>
+
+//<Navbar functions>
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -27,3 +23,31 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+//</Navbar functions>
+
+// <Scroll smooth>
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 700, function () {
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+// </Scroll smooth>
